6e7bae0d2705c638b8a837e875e837dd30a53e82
masriant <ssostiami@gmail.com>
2020-07-19 03:32:19 +0700

    Database Faker

---
+++ app/Database/Seeds/UsersSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\I18n\Time;
+
+
+class UsersSeeder extends \CodeIgniter\Database\Seeder
+{
+  public function run()
+  {
+    // $data = [
+    //   [
+    //     'nama' => 'Masrianto SAB',
+    //     'alamat'    => 'Jl. Serdang Baru Raya No. 4B - Bandung',
+    //     'created_at' => Time::now(),
+    //     'updated_at' => Time::now(),
+    //   ],
+    //   [
+    //     'nama' => 'Hasan',
+    //     'alamat'    => 'Jl. Taruna Jaya No. 44 - Solo',
+    //     'created_at' => Time::now(),
+    //     'updated_at' => Time::now(),
+    //   ],
+    //   [
+    //     'nama' => 'Zainal',
+    //     'alamat'    => 'Jl. Serdang Baru IX No. 4B - Makassar',
+    //     'created_at' => Time::now(),
+    //     'updated_at' => Time::now(),
+    //   ]
+    // ];
+
+    // coba faker
+    // generate data by accessing properties
+    // echo $faker->name;
+    $faker = \Faker\Factory::create('id_ID');
+    for ($i = 0; $i < 150; $i++) {
+      $data = [
+        'nama'            => $faker->name,
+        'username'        => $faker->userName,
+        'email'           => $faker->freeEmail,
+        'password'        => $faker->regexify('[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}'), // sm0@y8k96a.ej
+        'alamat'          => $faker->address,
+        'level'           => $faker->randomNumber(1),
+        'created_at'      => Time::createFromTimestamp($faker->unixTime()),
+        'updated_at'      => Time::now(),
+      ];
+      $this->db->table('users')->insert($data);
+    }
+
+
+    // Simple Queries
+    // $this->db->query(
+    //   "INSERT INTO orang (nama, alamat,created_at,updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)",
+    //   $data
+    // );
+
+    // Using Query Builder
+    // $this->db->table('orang')->insert($data);
+    // $this->db->table('orang')->insertBatch($data);
+  }
+}
