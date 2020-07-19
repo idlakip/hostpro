8300e741abaa5795d7ede53251a3ed1c5622611d
idlakip <67411936+idlakip@users.noreply.github.com>
2020-07-19 17:16:55 +0700

    Template admin lte (#3)
    * Database Faker
    
    * Menggunakan Faker Random
    
    * ok
    
    * Add Template Admin LTE
    
    * Struktur Template
    
    * add home & about
    
    * Struktur Template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,71 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data = [
+			'title' => 'Home',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
 	}
 
 	//--------------------------------------------------------------------
 
+	public function table()
+	{
+		$data = [
+			'title' => 'Tabel',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+	public function map()
+	{
+		$data = [
+			'title' => 'Map',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function charts()
+	{
+		$data = [
+			'title' => 'Charts',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+
+	public function contact()
+	{
+		$data = [
+			'title' => 'Home',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function about()
+	{
+		$data = [
+			'title' => 'About',
+			'name' => 'User',
+			'isi' => 'about'
+		];
+		echo view('layout/templates', $data); // templates 2
+	}
 }
---
+++ app/Views/layout/data_map.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $title; ?></title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?= base_url(); ?>/template/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?= base_url(); ?>/template/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+
+  <?= $this->include('layout/theme/m_navtop'); ?>
+  <?= $this->include('layout/theme/m_sidebar'); ?>
+  <div class="content-wrapper">
+    <?= $this->include('layout/theme/m_wrapper'); ?>
+    <?= $this->renderSection('content'); ?>
+
+    </section>
+  </div>
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.1.0-pre
+    </div>
+    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery -->
+  <script src="<?= base_url(); ?>/template/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="<?= base_url(); ?>/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="<?= base_url(); ?>/template/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="<?= base_url(); ?>/template/dist/js/demo.js"></script>
+</body>
+
+</html>
