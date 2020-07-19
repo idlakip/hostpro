e009e31dd78df2e803b617aecee055b352e5aa79
masriant <ssostiami@gmail.com>
2020-07-19 17:06:59 +0700

    Struktur Template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,14 +6,66 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		// return view('welcome_message');
-		return view('home');
+		$data = [
+			'title' => 'Home',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
 	}
 
 	//--------------------------------------------------------------------
+
+	public function table()
+	{
+		$data = [
+			'title' => 'Tabel',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+	public function map()
+	{
+		$data = [
+			'title' => 'Map',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function charts()
+	{
+		$data = [
+			'title' => 'Charts',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+
+
+	public function contact()
+	{
+		$data = [
+			'title' => 'Home',
+			'name' => 'Masrianto'
+		];
+		return view('home', $data);
+	}
+
+	//--------------------------------------------------------------------
+
 	public function about()
 	{
-		// return view('welcome_message');
-		return view('about');
+		$data = [
+			'title' => 'About',
+			'name' => 'User',
+			'isi' => 'about'
+		];
+		echo view('layout/templates', $data); // templates 2
 	}
 }
